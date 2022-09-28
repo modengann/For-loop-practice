f3a9c1613613effe13d9b51a36f08200f4857544
CaseyGann <cmoden@gannacademy.org>
2022-09-28 09:51:26 -0400

    init

---
+++ src/App.java
@@ -0,0 +1,16 @@
+/** YouTube explanation of for-loop: https://youtu.be/mTRCf0zXAp4
+ * 
+ * Task:
+ * Look back to Exercise 32 - The sum of a set of numbers from Chapter 11
+ * If you didn't complete this assignment, choose one nearby that used loops
+ * We completed this assignment using a while-loop (most likely).
+ * Re-code this assignment using a for-loop. How can we translate our ideas from while-loops to for-loops?
+ * 
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+
+    }
+}
