0c72ef7c93fd3b015948a3486c3dc56b95d6ebba
modengann <99417989+modengann@users.noreply.github.com>
2023-03-20 10:59:51 -0400

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,10 +1,7 @@
 /** YouTube explanation of for-loop: https://youtu.be/mTRCf0zXAp4
  * 
  * Task:
- * Look back to Exercise 32 - The sum of a set of numbers from Chapter 11
- * If you didn't complete this assignment, choose one nearby that used loops
- * We completed this assignment using a while-loop (most likely).
- * Re-code this assignment using a for-loop. How can we translate our ideas from while-loops to for-loops?
+ * Use a for-loop to create code that will print all the even numbers starting at 2 and going up to 20 (including 20)
  * 
  */
 
